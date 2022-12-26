74914bf3a4775233ecc9a5b233e867b545c223d2
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2022-12-26 04:41:13 +0000

    Add files via upload

--- benchmark_ip_ranger.py
+++ benchmark_ip_ranger.py
@@ -21,8 +21,24 @@ import os.path
 import time
 import module_ip_ranger
 t0 = time.time()
-import module_0_0_0_0_to_0_255_255_255_pythonic_list
-print('import module_0_0_0_0_to_0_255_255_255_pythonic_list time:', time.time() - t0)
+import module_0_0_0_0_to_0_255_255_255                  #1
+import module_10_0_0_0_to_10_255_255_255                #2
+import module_100_64_0_0_to_100_127_255_255
+import module_127_0_0_0_to_127_255_255_255
+import module_169_254_0_0_to_169_254_255_255            #5
+import module_172_16_0_0_to_172_31_255_255
+import module_192_0_0_0_to_192_0_0_255                  #7
+import module_192_0_2_0_to_192_0_2_255                  #8
+import module_192_88_99_0_to_192_88_99_255              #9
+import module_192_168_0_0_to_192_168_255_255
+import module_198_18_0_0_to_198_19_255_255
+import module_198_51_100_0_to_198_51_100_255            #12
+import module_203_0_113_0_to_203_0_113_255              #13
+# import module_224_0_0_0_to_239_255_255_255
+import module_233_252_0_0_to_233_252_0_255              #15
+# import module_240_0_0_0_to_255_255_255_254
+import module_255_255_255_255                           #17
+print('import address ranges time:', time.time() - t0)
 
 f = {'./module_0_0_0_0_to_0_255_255_255.txt': module_ip_ranger.compile_0_0_0_0_to_0_255_255_255,
      './module_10_0_0_0_to_10_255_255_255.txt': module_ip_ranger.compile_10_0_0_0_to_10_255_255_255,
@@ -37,9 +53,9 @@ f = {'./module_0_0_0_0_to_0_255_255_255.txt': module_ip_ranger.compile_0_0_0_0_t
      './module_198_18_0_0_to_198_19_255_255.txt': module_ip_ranger.compile_198_18_0_0_to_198_19_255_255,
      './module_198_51_100_0_to_198_51_100_255.txt': module_ip_ranger.compile_198_51_100_0_to_198_51_100_255,
      './module_203_0_113_0_to_203_0_113_255.txt': module_ip_ranger.compile_203_0_113_0_to_203_0_113_255,
-     './module_224_0_0_0_to_239_255_255_255.txt': module_ip_ranger.compile_224_0_0_0_to_239_255_255_255,
+     # './module_224_0_0_0_to_239_255_255_255.txt': module_ip_ranger.compile_224_0_0_0_to_239_255_255_255,
      './module_233_252_0_0_to_233_252_0_255.txt': module_ip_ranger.compile_233_252_0_0_to_233_252_0_255,
-     './module_240_0_0_0_to_255_255_255_254.txt': module_ip_ranger.compile_240_0_0_0_to_255_255_255_254,
+     # './module_240_0_0_0_to_255_255_255_254.txt': module_ip_ranger.compile_240_0_0_0_to_255_255_255_254,
      './module_255_255_255_255.txt': module_ip_ranger.compile_255_255_255_255}
 
 
@@ -53,16 +69,12 @@ def benchmark_enum(data_enum=[]):
     print('enumeration time:', time.time() - t0)
 
 
-def benchmark_read(file=str):
+def benchmark_read(data_enum=[]):
     t0 = time.time()
     try:
-        l = []
-        with open(file, 'r') as fo:
-            for line in fo:
-                line = line.strip()
-                l.append(line)
-        print('items: ' + str(len(l)))
-        print('read time:', time.time() - t0)
+        t0 = time.time()
+        print('items: ' + str(len(data_enum(disk=True))))
+        print('enumeration time:', time.time() - t0)
     except:
         print('file: not not yet exists.')
         pass
@@ -81,10 +93,59 @@ def benchmark_all():
             print('benchmarking:', f[k])
             data_enum = f[k]
             benchmark_enum(data_enum=data_enum)
-            benchmark_read(file=k)
+            benchmark_read(data_enum=data_enum)
+
             if i == 0:
                 t0 = time.time()
-                benchmark_pythonic_list(data_enum=module_0_0_0_0_to_0_255_255_255_pythonic_list.reserved_ipv4, t0=t0)
+                benchmark_pythonic_list(data_enum=module_0_0_0_0_to_0_255_255_255.reserved_ipv4, t0=t0)
+            elif i == 1:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_10_0_0_0_to_10_255_255_255.reserved_ipv4, t0=t0)
+            elif i == 2:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_100_64_0_0_to_100_127_255_255.reserved_ipv4, t0=t0)
+            elif i == 3:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_127_0_0_0_to_127_255_255_255.reserved_ipv4, t0=t0)
+            elif i == 4:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_169_254_0_0_to_169_254_255_255.reserved_ipv4, t0=t0)
+            elif i == 5:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_172_16_0_0_to_172_31_255_255.reserved_ipv4, t0=t0)
+            elif i == 6:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_192_0_0_0_to_192_0_0_255.reserved_ipv4, t0=t0)
+            elif i == 7:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_192_0_2_0_to_192_0_2_255.reserved_ipv4, t0=t0)
+            elif i == 8:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_192_88_99_0_to_192_88_99_255.reserved_ipv4, t0=t0)
+            elif i == 9:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_192_168_0_0_to_192_168_255_255.reserved_ipv4, t0=t0)
+            elif i == 10:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_198_18_0_0_to_198_19_255_255.reserved_ipv4, t0=t0)
+            elif i == 11:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_198_51_100_0_to_198_51_100_255.reserved_ipv4, t0=t0)
+            elif i == 12:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_203_0_113_0_to_203_0_113_255.reserved_ipv4, t0=t0)
+            # elif i == 13:
+            #     t0 = time.time()
+            #     benchmark_pythonic_list(data_enum=module_224_0_0_0_to_239_255_255_255.reserved_ipv4, t0=t0)
+            elif i == 14:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_233_252_0_0_to_233_252_0_255.reserved_ipv4, t0=t0)
+            # elif i == 15:
+            #     t0 = time.time()
+            #     benchmark_pythonic_list(data_enum=module_240_0_0_0_to_255_255_255_254.reserved_ipv4, t0=t0)
+            elif i == 16:
+                t0 = time.time()
+                benchmark_pythonic_list(data_enum=module_255_255_255_255.reserved_ipv4, t0=t0)
         i += 1
 
 
